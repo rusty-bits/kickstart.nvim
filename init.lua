cb351593cb1bc929d1ae652161dec9f0471c6786
rusty-bits <rusty_bits@aol.com>
2026-01-03 13:15:00 -0500

    colorscheme to tomorrow-night

--- init.lua
+++ init.lua
@@ -947,7 +947,7 @@ require('lazy').setup({
   },
 })
 
-vim.cmd.colorscheme 'tomorrow-night-eighties'
+vim.cmd.colorscheme 'tomorrow-night'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
